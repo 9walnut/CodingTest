5e2c04154c0e9f83c409393ad1aa62c81cc3014b
9walnut <kguho9202@gmail.com>
2023-09-20 21:03:00 +0900

    Day3 renew

--- python_backjoon/Day3.py
+++ python_backjoon/Day3.py
@@ -20,3 +20,37 @@ else:
     print('0')
 
 # 백준 10039 - 평균 점수
+result = 0
+for i in range(5):
+    score = int(input())
+    if score < 40:
+        result += 40
+    else:
+        result += score
+
+print(int(result/5))
+
+# 백준 10103 - 주사위
+n = int(input())
+C = 100
+S = 100
+
+for i in range(1, n+1):
+    a, b = map(int, input().split())
+    if a > b:
+        S -= a
+    elif a < b:
+        C -= b
+
+print(C)
+print(S)
+
+# 백준 5717 - 상근이의 친구들
+# 이거 왜 전역 변수면 시간 초과 나는지?
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    else:
+        print(a+b)
