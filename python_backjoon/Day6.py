aaba8ef3a7ade2c88f7a460cbc4d79f7ed02f7ad
9walnut <kguho9202@gmail.com>
2023-09-23 19:35:14 +0900

    Day6

--- python_backjoon/Day6.py
+++ python_backjoon/Day6.py
@@ -2,21 +2,18 @@
 n = int(input())
 
 for i in range(1, n+1):
-  print(" "*(i-1)+"*"*(n+1-i))
+    print(" "*(i-1)+"*"*(n+1-i))
 
-  # 0 5
-  # 1 4
+    # 0 5
+    # 1 4
 
-  # 백준 3052 - 나머지
-  n = int(input())
+    # 백준 3052 - 나머지
+    n = int(input())
 
-  result = 0;
+    result = 0
 
 # 백준 - 2442 - 별찍기5
 t = int(input())
 
 for i in range(1, t+1):
-  print(" "*(t-i)+"*"*(2*t-1)+" "*(t-i))
-
-    *    
-  
+    print(" "*(t-i) + "*"*(2*i-1))
