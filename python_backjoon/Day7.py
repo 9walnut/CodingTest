1155d9ba112d834fa7e67728161bbfbd72416c6b
9walnut <kguho9202@gmail.com>
2023-09-25 00:18:53 +0900

    Day7

--- python_backjoon/Day7.py
+++ python_backjoon/Day7.py
@@ -1,1 +1,14 @@
-# 백준
+# 백준 2444 - 별찍기7
+t = int(input())
+
+for i in range(1, t+1):
+    print(" "*(t-i) + "*"*(2*i-1))
+for i in range(t-1, 0, -1):
+    print(" "*(t-i) + "*"*(2*i-1))
+
+
+# 백준 2443 - 별찍기6
+t = int(input())
+
+for i in range(t, 0, -1):
+    print(" "*(t-i) + "*"*(2*i-1))
