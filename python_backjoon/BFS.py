9a726973102a3c2a3edad2b1b1fb41c83db60c12
9walnut <kguho9202@gmail.com>
2023-10-06 15:08:42 +0900

    DFS

---
+++ python_backjoon/BFS.py
@@ -0,0 +1,44 @@
+# 16173 - 점프왕 젤리
+from collections import deque
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+# 방문 유무 확인
+visited = [[False]*N for _ in range(N)]
+
+# 오른쪽만 가능
+dx = [1, 0]
+# 아래만 가능
+dy = [0, 1]
+
+
+def bfs(x, y):
+    q = deque()
+    q.append([x, y])
+
+    while q:
+        x, y = q.popleft()
+        # 밟고 있는 칸의 숫자 인출
+        step = graph[x][y]
+
+        # 끝점이 -1이므로 -1로 도달하면 성공
+        if graph[x][y] == -1:
+            return True
+        for move in range(2):
+            # 밟고 있는 숫자를 인출해서 이동수에 곱해서 이동
+            nx = x+dx[move]*step
+            ny = y+dy[move]*step
+        # 주어진 범위 벗어나는 경우
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                continue
+            # 방문하지 않았을 경우 방문 수정
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                # q에 담아주면서 반복
+                q.append([nx, ny])
+
+
+if bfs(0, 0):
+    print('HaruHaru')
+else:
+    print("Hing")
--- python_backjoon/암기.py
+++ python_backjoon/암기.py
@@ -1,7 +1,21 @@
+# 1차원 리스트
+# N개의 원소가 0으로 초기화
+L = [0] * n
+# N개의 원소가 True
+L = [True]*N
 # 공백 기준 입력받기
 n, m = map(int, input().split())
 
-# 2차원 배열 선언
+# 2차원 배열 선언(문자열 입력받아 그래프)
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
+
+# N*N 행렬을 0으로 초기화
+board = [[0]*(N) for _ in range(N)]
+
+# row*col행렬을 0으로 초기화
+board2 = [[0]*(col) for _ in range(row)]
+
+# 그래프 초기화
+graph = [[] for i in range(n+1)]
