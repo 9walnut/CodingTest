296a85a932c2165148ecd6c8a3f33aad9bb9bad6
9walnut <kguho9202@gmail.com>
2023-10-18 10:36:58 +0900

    BFS-4

--- python_backjoon/BFS.py
+++ python_backjoon/BFS.py
@@ -1,4 +1,5 @@
 # 16173 - 점프왕 젤리
+import sys
 from collections import deque
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
@@ -155,12 +156,71 @@ print(len(ans), *ans, sep='\n')
 
 # 2178 - 미로탐색
 
-
 a, b = map(int, input().split())
-graph = [[for i in range(b)] for i in range(a)]
-visited = [[0]*b for _ in range(a)]
+graph = [list(map(int, input())) for _ in range(a)]
 
 
+def bfs(si, sj, ei, ej):
+    q = []
+    visited = [[0]*b for _ in range(a)]
+    q.append((si, sj))
+    visited[si][sj] = 1
+
+    while q:
+        ci, cj = q.pop(0)
+        # 목표지점 - 정답처리가 필요한 경우 이 자리에
+        if (ci, cj) == (ei, ej):
+            return visited[ei][ej]
+        # 4방향, 범위내, 조건에 맞으면 arr = 1이고 방문 == 0
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            ni, nj = ci+di, cj+dj
+            if 0 <= ni < a and 0 <= nj < b and visited[ni][nj] == 0 and graph[ni][nj] == 1:
+                q.append((ni, nj))
+                visited[ni][nj] = visited[ci][cj] + 1
 
 
+answer = bfs(0, 0, a-1, b-1)
+print(answer)
 
+# 7569 - 토마토(3차원)
+# 모두 익는데 걸리는 날짜
+# 하나라도 못익으면 -1 return
+# 익지않은 개수 cnt로 하고 0보다 크면 -1 return하게끔 조건
+#  h > i > j 순으로 전체 순회해서 1ㅣ면 q에 삽입
+# 0이면 cnt +1
+
+
+def bfs():
+    q = deque()
+    visited = [[[0]*M for _ in range(N)] for _ in range(H)]
+    cnt = 0
+    # 전체 순회해서 q에 초기 데이터 삽입
+    for h in range(H):
+        for i in range(N):
+            for j in range(M):
+                if graph[h][i][j] == 1:  # 익은 토마토
+                    q.append((h, i, j))
+                    visited[h][i][j] = 1
+                elif graph[h][i][j] == 0:  # 안익은 토마토
+                    cnt += 1
+
+    while q:
+        ch, ci, cj = q.popleft()
+        for dh, di, dj in ((0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1), (-1, 0, 0), (1, 0, 0)):
+            nh, ni, nj = ch+dh, ci+di, cj+dj
+            # 안익으면 토마토면 익어서 전파
+            if 0 <= ni < N and 0 <= nj < M and 0 <= nh < H and visited[nh][ni][nj] == 0 and graph[nh][ni][nj] == 0:
+                q.append((nh, ni, nj))
+                visited[nh][ni][nj] = visited[ch][ci][cj] + 1
+                cnt -= 1  # 안익은 토마토 익은 토마토로 바꾸면서 -1
+
+    if cnt == 0:
+        return visited[ch][ci][cj]-1
+    else:
+        return -1
+
+
+M, N, H = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(N)]for _ in range(H)]
+answer = bfs()
+print(answer)
