8ca9cef54fd7280993a1187aba0c1c6ceb194d3f
9walnut <kguho9202@gmail.com>
2024-03-07 18:57:31 +0900

    day10

---
+++ python_backjoon/Programmers/day10.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def solution(numbers):
+    answer = 0
+    num_set = set()
+
+    for i in range(1, len(numbers) + 1):
+        perms = permutations(numbers, i)
+        for perm in perms:
+            num_set.add(int(''.join(perm)))
+
+    for num in num_set:
+        if is_prime(num):
+            answer += 1
+
+    return answer
