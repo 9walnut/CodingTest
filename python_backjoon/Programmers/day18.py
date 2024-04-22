d4a382231513250dae079673d0d150563950d85b
9walnut <kguho9202@gmail.com>
2024-04-22 13:25:53 +0900

    shark

--- python_backjoon/Programmers/day18.py
+++ python_backjoon/Programmers/day18.py
@@ -1,3 +1,4 @@
+import sys
 from collections import deque
 
 
@@ -112,11 +113,12 @@ def solution(N, number):
         else:  # N을 8번 사용했는데도 찾고자하는 값 number가 존재하지 않는다면 -1 return
             answer = -1
     return answer
-import sys
+
 
 N, L = map(int, sys.stdin.readline().split())
 
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
 
 def check(line, L):
     # 경사로 생기는 곳 체크
@@ -131,7 +133,7 @@ def check(line, L):
             return False
         # 현재 높이가 다음 높이 보다 높으면 오른쪽 높이가 같은지 체크
         elif line[i] > line[i+1]:
-            temp = line[i+1] # 다음 높이
+            temp = line[i+1]  # 다음 높이
             for j in range(i+1, i+L+1):
                 # 경사 길이가 범위 안이면
                 if 0 <= j < N:
@@ -165,6 +167,7 @@ def check(line, L):
                     return False
     return True
 
+
 answer = 0
 # 가로 길 체크
 for i in board:
@@ -180,3 +183,67 @@ for i in range(N):
         answer += 1
 
 print(answer)
+
+
+N = int(input())
+
+dx = [-1, 0, 0, 1]  # 상 좌 우 하
+dy = [0, -1, 1, 0]
+room = []
+sharksize = 2
+sharkeat = 0
+
+for i in range(N):
+    room.append([int(x) for x in sys.stdin.readline().rstrip().split()])
+    for j in range(len(room[i])):
+        if room[i][j] == 9:
+            room[i][j] = 0
+            shark_x, shark_y = i, j
+
+
+def finding_fish(sx, sy):
+    global sharksize
+    deq = deque()
+    deq.append([sx, sy])
+
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    distance = [[0 for _ in range(N)] for _ in range(N)]
+    can_eat_fish = []
+
+    while deq:
+        x, y = deq.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if room[nx][ny] <= sharksize and not visited[nx][ny]:  # 이동이 가능하면
+                    visited[nx][ny] = True
+                    distance[nx][ny] = distance[x][y] + 1
+                    deq.append([nx, ny])
+
+                    if room[nx][ny] < sharksize and room[nx][ny] != 0:  # 물고기가 있고 그걸 먹을 수 있다면
+                        can_eat_fish.append([nx, ny, distance[nx][ny]])
+
+    can_eat_fish.sort(key=lambda x: (x[2], x[0], x[1]))  # 정렬은 거리, x, y 오름차순으로
+    return can_eat_fish
+
+
+if __name__ == '__main__':
+    ans = 0
+
+    while True:
+        fishlist = finding_fish(shark_x, shark_y)
+
+        if len(fishlist) == 0:  # 먹을 수 있는 물고기가 없다면
+            print(ans)
+            exit(0)
+
+        shark_x, shark_y, move_time = fishlist[0]
+
+        sharkeat += 1
+        if sharksize == sharkeat:  # 먹은 물고기수와 사이즈가 같다면
+            sharkeat = 0
+            sharksize += 1
+
+        room[shark_x][shark_y] = 0  # 물고기 먹은 자리는 빈칸으로 바꿈
+        ans += move_time
