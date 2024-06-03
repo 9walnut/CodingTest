379c49c0a5c762f67f4827bd4946cc4cc7af9ce8
9walnut <kguho9202@gmail.com>
2024-06-03 14:21:02 +0900

    ✨ Add index differences
    add index differeneces

--- python_backjoon/antlabs.py
+++ python_backjoon/antlabs.py
@@ -1,37 +1,82 @@
-# ANTlabs test
 import difflib
 
 
-def highlight_differences(str1, str2):
-    diff = difflib.ndiff(str1, str2)
+def highlight_differences2(str1, str2):
+    result_str1 = ""
+    result_str2 = ""
+
+    # Determine the minimum length to avoid index out of range issues
+    min_len = min(len(str1), len(str2))
+
+    # Iterate over both strings and compare characters
+    for i in range(min_len):
+        if str1[i] == str2[i]:
+            result_str1 += str1[i]
+            result_str2 += str2[i]
+        else:
+            result_str1 += f'<span style="background-color: pink;">{
+                str1[i]}</span>'
+            result_str2 += f'<span style="background-color: lightblue;">{
+                str2[i]}</span>'
+
+    # Handle the remaining part of the longer string
+    if len(str1) > min_len:
+        for i in range(min_len, len(str1)):
+            result_str1 += f'<span style="background-color: pink;">{
+                str1[i]}</span>'
+    if len(str2) > min_len:
+        for i in range(min_len, len(str2)):
+            result_str2 += f'<span style="background-color: lightblue;">{
+                str2[i]}</span>'
+
+    return result_str1, result_str2
+
+
+def highlight_differences_with_index(str1, str2):
+    sm = difflib.SequenceMatcher(None, str1, str2)
 
     result_str1 = ""
     result_str2 = ""
 
-    for i, s in enumerate(diff):
-        if s.startswith("  "):
-            result_str1 += s[2]
-            result_str2 += s[2]
-        elif s.startswith("- "):
-            result_str1 += f'<span style="background-color: pink;">{
-                s[2]}</span>'
-        elif s.startswith("+ "):
-            result_str2 += f'<span style="background-color: lightblue;">{
-                s[2]}</span>'
+    for opcode, i1, i2, j1, j2 in sm.get_opcodes():
+        if opcode == 'equal':
+            result_str1 += str1[i1:i2]
+            result_str2 += str2[j1:j2]
+        elif opcode == 'replace':
+            for k in range(i1, i2):
+                result_str1 += f'<span style="background-color: pink;">{
+                    str1[k]}</span>'
+            for k in range(j1, j2):
+                result_str2 += f'<span style="background-color: lightblue;">{
+                    str2[k]}</span>'
+        elif opcode == 'delete':
+            for k in range(i1, i2):
+                result_str1 += f'<span style="background-color: pink;">{
+                    str1[k]}</span>'
+        elif opcode == 'insert':
+            for k in range(j1, j2):
+                result_str2 += f'<span style="background-color: lightblue;">{
+                    str2[k]}</span>'
 
     return result_str1, result_str2
 
 
 def generate_html(str1, str2):
-    highlighted_str1, highlighted_str2 = highlight_differences(str1, str2)
+    highlighted_str1, highlighted_str2 = highlight_differences2(str1, str2)
+    highlighted_index_str1, highlighted_index_str2 = highlight_differences_with_index(
+        str1, str2)
 
     html = f"""
     <html>
     <body>
-        <p>First String:</p>
+        <p>First String (differences highlighted):</p>
         <p>{highlighted_str1}</p>
-        <p>Second String:</p>
+        <p>Second String (differences highlighted):</p>
         <p>{highlighted_str2}</p>
+        <p>First String (differences with index highlighted):</p>
+        <p>{highlighted_index_str1}</p>
+        <p>Second String (differences with index highlighted):</p>
+        <p>{highlighted_index_str2}</p>
     </body>
     </html>
     """
